36814ce64d1f34ae1a48ad9d3ac5af1128bdcb5d
phkknning25-beep <phkknning25@gmail.com>
2026-06-01 11:54:18 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,14 @@
-```jsx
-
-import React from 'react'
-
-import ReactDOM from 'react-dom/client'
-
-import './index.css'
-
-import App from './App.jsx'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-
-  <React.StrictMode>
-
-    <App />
-
-  </React.StrictMode>,
-
-)
-
-``` 
-
+<!doctype html>
+<html lang="th">
+  <head>
+    <meta charset="UTF-8" />
+    <link rel="icon" type="image/svg+xml" href="/vite.svg" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>V-Permission</title>
+    <link rel="stylesheet" href="./src/index.css">
+  </head>
+  <body>
+    <div id="root"></div>
+    <script type="module" src="./src/main.jsx"></script>
+  </body>
+</html>
